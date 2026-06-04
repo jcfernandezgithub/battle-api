7272bdb69d3f74529f0bcade9cfc633e58ac72b6
Jorge Cristian Fernandez <jcfernandez@outlook.com.ar>
2026-06-03 21:27:15 -0300

    imports

--- src/@realtime/realtime.module.ts
+++ src/@realtime/realtime.module.ts
@@ -3,7 +3,11 @@ import { Module } from '@nestjs/common';
 import { RealtimeGateway } from './realtime.gateway';
 
 @Module({
-  providers: [RealtimeGateway],
-  exports: [RealtimeGateway],
+  providers: [
+    RealtimeGateway,
+  ],
+  exports: [
+    RealtimeGateway,
+  ],
 })
-export class RealtimeModule {}
+export class RealtimeModule { }
--- src/modules/events/event.module.ts
+++ src/modules/events/event.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 import { SupabaseModule } from 'src/@supabase/supabase.module';
+import { RealtimeGateway } from 'src/@realtime/realtime.gateway';
 
 @Module({
   imports: [SupabaseModule],
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [EventsService, RealtimeGateway],
   exports: [EventsService],
 })
-export class EventsModule {}
+export class EventsModule { }
